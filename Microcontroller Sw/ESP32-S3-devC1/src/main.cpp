a1f28f82c57e3204f0bb42cd4fda5448e9cbade7
redto0 <aboccacc@umich.edu>
2026-04-02 13:59:40 -0400

    wifi stuff

--- Microcontroller Sw/ESP32-S3-devC1/src/main.cpp
+++ Microcontroller Sw/ESP32-S3-devC1/src/main.cpp
@@ -1,11 +1,57 @@
 #include <Arduino.h>
+#include <freertos/queue.h> // #include <pico/util/queue.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+#include "secrets.h" 
 
-// put function declarations here:
-int myFunction(int, int);
+// ==========================================
+//    ESP cpp
+// ==========================================
+
+//  DATA STRUCTURE & QUEUE SETUP
+// Define the shape of the data you want to pass between cores
+typedef struct {
+  int sensorValue;
+  unsigned long timestamp;
+} CoreMessage;
+
+// Create the global queue so both cores can access it
+queue_t coreQueue;
+
+// MQTT SETUP 
+WiFiClient picoClient;
+PubSubClient client(picoClient);
+
+void reconnet() {
+  // loop until we're reconnected for. Ideally not blockinf core one. 
+  while ((!cilent.connect())){
+    /* code */
+    Serial.print("[Core 0] Attempting MQTT connection...");
+    String clientId = "Esp32_Cleint-";
+    clientId += String(random(0xffff), HEX);
+
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+    } else {
+      Serial.print("Failed, rc=");
+      Serial.print(client.state());
+      serial.println(" trying again");
+      delay(5000);
+    }
+  }
+  
+}
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);  // bestest baugh rate. 
+  delay(2000); // Giving the serial monitor a moment to connect
+
+  // Initialize the queue: (pointer to queue, size of one item, max items)
+  queue_init(&coreQueue, sizeof(CoreMessage), 10);
+
+
 }
 
 void loop() {
