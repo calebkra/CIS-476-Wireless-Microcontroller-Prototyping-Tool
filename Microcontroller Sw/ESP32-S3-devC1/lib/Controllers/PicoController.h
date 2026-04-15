18716b04cbf39d9f728e57b5dd4cc1712117e24e
redto0 <aboccacc@umich.edu>
2026-04-15 00:40:49 -0400

    pin controllers update

--- Microcontroller Sw/ESP32-S3-devC1/lib/Controllers/PicoController.h
+++ Microcontroller Sw/ESP32-S3-devC1/lib/Controllers/PicoController.h
@@ -4,6 +4,11 @@
 #include "IController.h"
 
 class PicoController : public IController {
+private:
+    // array for all pins to store last state, This is too send all states without
+    // locking having to poll the other cores. 
+    int _lastState[30] = {0}; 
+
 public:
     int readPWM(int pin) override;
     void writePWM(int pin, float value) override;
